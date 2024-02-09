af21783f34de93587bc914300af6ae9424698cd7
ibrahimputun <ibrahimputun01@gmail.com>
2024-02-09 21:01:19 +0300

    LoginForm

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -1,11 +1,68 @@
 import React from 'react'
+import { Form } from "formik";
+import { object, string } from "yup";
+import { Box, Button, TextField } from '@mui/material';
 
-const LoginForm = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
 
-export default LoginForm
+  export const loginSchema = object({
+    email: string()
+      .email("Lütfen geçerli bir email giriniz")
+      .required("Email girişi zorunludur"),
+    password: string()
+      .required("Şifre zorunludur.")
+      .min(8, "Şifre en az 8 karakter içermelidir")
+      .max(16, "Şifre en falza 16 karakter içermelidir")
+      .matches(/\d+/, "Şifre en az bir rakam içermelidir")
+      .matches(/[a-z]/, "Şifre en az bir küçük harf içermelidir")
+      .matches(/[A-Z]/, "Şifre en az bir büyük harf içermelidir")
+      .matches(
+        /[@$!%*?&]+/,
+        "Şifre en az bir özel karakter (@$!%*?&) içermelidir"
+      ),
+  })
+
+  const LoginForm = ({
+    values,
+    handleChange,
+    errors,
+    touched,
+    handleBlur,
+  }) => {
+    return (
+      <Form>
+        <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+          <TextField
+            label="Email"
+            name="email"
+            id="email"
+            type="text"
+            variant="outlined"
+            value={values.username}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={touched.username && Boolean(errors.username)}
+            helperText={errors.username}
+          />
+          
+          <TextField
+            label="Password"
+            name="password"
+            id="password"
+            type="password"
+            variant="outlined"
+            value={values.password}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={touched.password && Boolean(errors.password)}
+            helperText={errors.password}
+          />
+          <Button type="submit" variant="contained" size="large">
+            Submit
+          </Button>
+        </Box>
+      </Form>
+    );
+  };
+  
+  export default LoginForm;
+  
