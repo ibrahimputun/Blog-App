13d8737c16e4c044a6292bb39af19ee168cc2f50
ibrahimputun <ibrahimputun01@gmail.com>
2024-02-09 21:01:35 +0300

    authSlice

--- src/features/authSlice.jsx
+++ src/features/authSlice.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import { createSlice } from "@reduxjs/toolkit"
 
-const authSlice = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const initialState = {
+  user: "",
+  loading: false,
+  error: false,
+  token: "",
 }
 
-export default authSlice
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    fetchStart: (state) => {
+      state.loading = true
+    },
+    loginSuccess: (state, { payload }) => {
+      state.loading = false
+      state.user = payload.user.username
+      state.token = payload.token
+    },
+    registerSuccess: (state, { payload }) => {
+      state.loading = false
+      state.user = payload.data.username
+      state.token = payload.token
+    },
+    logoutSuccess: (state) => {
+      state.user = ""
+      state.loading = false
+      state.token = ""
+    },
+    fetchFail: (state) => {
+      state.loading = false
+      state.error = true
+    },
+  },
+})
+
+export const {
+  fetchStart,
+  loginSuccess,
+  registerSuccess,
+  logoutSuccess,
+  fetchFail,
+} = authSlice.actions
+
+export default authSlice.reducer
+
