9a0d6cfbc504007128da102ed91f4b1d2feca46e
ibrahimputun <ibrahimputun01@gmail.com>
2024-02-09 21:01:56 +0300

    blogSlice

--- src/features/blogSlice.jsx
+++ src/features/blogSlice.jsx
@@ -1,11 +1,86 @@
-import React from 'react'
+import { createSlice } from "@reduxjs/toolkit";
 
-const blogSlice = () => {
-  return (
-    <div>
+const initialState = {
+  title: "",
+  imageUrl: "",
+  categories: [],
+  data: [],
+  loading: false,
+  error: false,
+  totalPages: 1,
+  currentPage: '',
+  nextPage: 1,
+  previousPage: false,
+  categoryId: "",
+  comments: [],
+  content: "",
+  likes: [],
+  updatedAt: "",
+  blogId: "",
+  userId: "",
+  firstname: "",
+  lastName: "",
+  username: "",
+  myblog : []
+};
+
+const blogSlice = createSlice({
+  name: "blog",
+  initialState,
+  reducers: {
+    fetchStart: (state) => {
+      state.error = false;
+      state.loading = true;
+    },
+    getBlogSuccess: (state, { payload }) => {
+      state.data = payload.data;
+      state.loading = false;
+      state.totalPages = payload.details.pages.total;
+      state.currentPage = payload.details.pages.current;
+      state.nextPage = payload.details.pages.next;
+      state.previousPage = payload.details.pages.previous;
+    },
+    getMyBlogSuccess: (state, { payload }) => {
+      state.myblog = payload.data;
+      state.loading = false;
+    },
+    getBlogCategorySuccess: (state, { payload }) => {
+      state.categories = payload.data;
+      state.loading = false;
+    },
+    fetchFail: (state) => {
+      state.loading = false;
+      state.error = true;
+    },
+    setBlogDetails: (state, { payload }) => {
+      state.imageUrl = payload.image;
+      state.title = payload.title;
+      state.loading = false;
+      state.error = false;
+      state.categoryId = payload.categoryId.name;
+      state.comments = payload.comments;
+      state.content = payload.content;
+      state.likes = payload.likes;
+      state.updatedAt = payload.categoryId.updatedAt;
+      state.blogId = payload._id;
+      state.userId = payload.userId._id;
+      state.firstname = payload.userId.firstname;
+      state.lastName = payload.userId.lastName;
+      state.username = payload.userId.username;
       
-    </div>
-  )
-}
+    },
+  },
+});
 
-export default blogSlice
+export const {
+  fetchStart,
+  getBlogSuccess,
+  getBlogCategorySuccess,
+  fetchFail,
+  setCurrentPage,
+  getMyBlogSuccess,
+  setBlogDetails,
+} = blogSlice.actions;
+
+export default blogSlice.reducer;
+
