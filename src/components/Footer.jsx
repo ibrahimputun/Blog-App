2447a44cb9209067e14c4c8dd8047efb3d5be21e
ibrahimputun <ibrahimputun01@gmail.com>
2024-02-14 21:41:46 +0300

    ilk commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Link from "@mui/material/Link";
+import { Box, Paper } from "@mui/material";
 
-const Footer = () => {
+export default function Footer() {
   return (
-    <div>
-      
-    </div>
-  )
+    <Paper
+    sx={{
+      marginTop: 'calc(10% + 60px)',
+      bottom: 0,
+      backgroundColor: 'purple',
+    }}
+    component="footer"
+    square
+    variant="outlined"
+  >
+    <Container maxWidth="lg" >
+  <Box
+  sx={{
+    flexGrow: 1,
+    justifyContent: "center",
+    display: "flex",
+    my: 1,
+    color: 'white', // Yeni eklenen satır
+    fontFamily: 'Arial',
+    textDecoration: 'none'
+  }}
+>
+  {"Copyright © "}
+  <Link color="inherit" href="https://mui.com/" style={{ marginRight: '10px' }}>
+    Your Website
+  </Link>
+  {new Date().getFullYear()}
+ 
+</Box>
+
+      <Box
+        sx={{
+          flexGrow: 1,
+          justifyContent: "center",
+          display: "flex",
+          mb: 2,
+        }}
+      >
+       
+      </Box>
+    </Container>
+  </Paper>
+);
 }
 
-export default Footer
+
+
+
