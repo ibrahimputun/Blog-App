bec02b40064115be6ec0d2706796efa42587bbbe
ibrahimputun <ibrahimputun01@gmail.com>
2024-02-18 09:31:18 +0300

    ilk commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -79,26 +79,9 @@ function ResponsiveAppBar() {
   const { user } = useSelector((state) => state.auth);
 
   return (
-    <AppBar position="static">
+    <AppBar position="static" sx={{ backgroundColor: "purple" }}>
       <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Typography
-            variant="h4"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: "none", md: "flex" },
-              fontFamily: "monospace",
-              fontWeight: 800,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-        
-          </Typography>
+        <Toolbar disableGutters>       
 
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <IconButton
@@ -131,35 +114,13 @@ function ResponsiveAppBar() {
             >
               {pages.map((page) => (
                 <MenuItem key={page.title}>
-                  <Typography
-                    onClick={() => navigate(page.url)}
-                    textAlign="center"
-                  >
-                    {page.title}
-                  </Typography>
+                  
                 </MenuItem>
               ))}
             </Menu>
           </Box>
           <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
        
-          </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
               <Button
